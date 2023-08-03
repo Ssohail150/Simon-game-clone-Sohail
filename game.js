6fee24b31afd0d4e9ea16f2ed80b1b625e0c69ce
Sohail <sayedsohail150@gmail.com>
2023-08-03 19:43:23 +0530

    Simon game clone Sohail

---
+++ game.js
@@ -0,0 +1,87 @@
+let buttonColours = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
+let randomNumber;
+var userClickedPattern = [];
+var started = false;
+var level = 0;
+
+$(document).keypress(function() {
+  if (!started) {
+
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+$(".btn").click(function () {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length-1);
+});
+
+function checkAnswer(currentLevel) {
+
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+
+    console.log("success");
+
+    if (userClickedPattern.length === gamePattern.length){
+
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+
+    }
+
+  } else {
+
+    console.log("wrong");
+
+    playSound("wrong");
+
+    $("body").addClass("game-over");
+      setTimeout(function () {
+        $("body").removeClass("game-over");
+      }, 200);
+
+      $("#level-title").text("Game Over, Press Any Key to Restart");
+
+      startOver();
+
+  }
+
+}
+
+function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("Level " + level);
+  randomNumber = Math.floor(Math.random() * buttonColours.length);
+  let randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour); // Fixed: Use randomChosenColour here instead of userChosenColour
+}
+
+function playSound(name) {
+  let audio = new Audio("sounds/" + name + ".mp3"); // Fixed: Use name here instead of randomChosenColour
+  audio.play();
+}
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
+
+function startOver() {
+
+  level = 0;
+  gamePattern = [];
+  started = false;
+}
